a22abae611a8d066ecc9897608e5ec9b7e8f29a3
3dhanushkrishna <3dhanushkccd@gmail.com>
2022-03-17 21:28:18 +0530

    Assessment java 4

---
+++ src/com/harman/Assessment4/Parent.java
@@ -0,0 +1,23 @@
+package com.harman.Assessment4;
+
+public class Parent {
+    public static void main(String[] args) {
+        subclass1 s1 = new subclass1();
+        s1.message();
+        subclass2 s2 = new subclass2();
+        s2.message();
+    }
+}
+abstract class Parents{
+    public abstract void message();
+}
+class subclass1 extends Parent{
+    public void message(){
+        System.out.println("This is first subclass");
+    }
+}
+class subclass2 extends Parent{
+    public void message(){
+        System.out.println("This is Second subclass");
+    }
+}
